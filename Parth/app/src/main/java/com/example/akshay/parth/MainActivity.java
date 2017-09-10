5e6db6fb416a3c4459fbe0d53a0ed96c31563cc6
Your Name <you@example.com>
2017-09-11 02:25:15 +0530

    login added

--- Parth/app/src/main/java/com/example/akshay/parth/MainActivity.java
+++ Parth/app/src/main/java/com/example/akshay/parth/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.akshay.parth;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void chat(View view) {
+        Intent i=new Intent(this,ChatActivity.class);
+        startActivity(i);
     }
 }
