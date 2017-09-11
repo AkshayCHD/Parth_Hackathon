bdfe2ff914d1d17e263eecc109077a95df8226ce
Your Name <you@example.com>
2017-09-11 07:47:23 +0530

    added ocr

--- Parth/app/src/main/java/com/example/akshay/parth/MainActivity.java
+++ Parth/app/src/main/java/com/example/akshay/parth/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.akshay.parth;
 
 import android.content.Intent;
 
+import android.net.Uri;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 
 import java.io.InputStream;
@@ -53,5 +55,23 @@ public class MainActivity extends AppCompatActivity {
         Intent i=new Intent(this,ChatActivity.class);
         startActivity(i);
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)) {
+
+            String phone = "08039514866";
+            Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null));
+            startActivity(intent);
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:" + phone ));
+            startActivity(callIntent);
+
+            return true;
+
+        }else
+            return super.onKeyDown(keyCode, event);
+    }
+
 
 }
